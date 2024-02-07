20f718ca258bfb41be0080573fa042a4d646224e
Levente <145211298+fulopl@users.noreply.github.com>
2024-02-07 10:09:46 +0100

    create folder structure

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+//import Book from "./model/Book";
+
+mongoose.connect(process.env.MONGO_DB_URL);
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+
+
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
