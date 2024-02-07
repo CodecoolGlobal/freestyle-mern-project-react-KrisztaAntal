6d68bd5e5ae8e450aed4fa932f8053fb80839e47
Adam Moór <moor.adam.is@gmail.com>
2024-02-07 11:35:16 +0100

    started working on itemlist

---
+++ client/book-worms/src/components/ItemList.jsx
@@ -0,0 +1,4 @@
+function ItemList(){
+    return (<h1>Helloh, list here!</h1>)
+}
+export default ItemList;
--- server/server.js
+++ server/server.js
@@ -9,12 +9,6 @@ const PORT = 5000;
 
 app.use(express.json());
 
-
-
-
-
-
 app.listen(PORT, ()=> {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
-
