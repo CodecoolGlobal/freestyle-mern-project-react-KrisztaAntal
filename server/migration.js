6a486f769ea2427365afbd485da2aef191e7fb33
Eszter Revesz <74403983+reveszter@users.noreply.github.com>
2024-02-07 14:53:41 +0100

    fetchData from API and save to MongoDB

--- server/migration.js
+++ server/migration.js
@@ -1,23 +1,28 @@
 import selfLinks1 from "./selfLinks.js"
+import Book from "./model/Book.js"
+import mongoose from "mongoose";
+import "dotenv/config";
+//import Book from "./model/Book";
 
+mongoose.connect(process.env.MONGO_DB_URL);
 
 async function fetchData() {
     const books = await Promise.all(selfLinks1.map(async (link, index) => {
         console.log(link, index);
         const response = await fetch(link)
         const data = await response.json()
-        console.log(data.volumeInfo.title);
-        const book = {
-            title: data.volumeInfo.title ? data.volumeInfo.title : null,
-            authors: data.volumeInfo.authors[0],
-            genre: data.volumeInfo.categories,
-            image: data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail : null,
-            detailURL: data.selfLink
-        }
-        return book;
+        const book = new Book({
+            BookId: data.id,
+            Title: data.volumeInfo.title ? data.volumeInfo.title : null,
+            Author: data.volumeInfo.authors[0],
+            Genre: data.volumeInfo.categories ? data.volumeInfo.categories[0] : null,
+            BookImage: data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail : null,
+            DeatailURL: data.selfLink,
+            Reviews: []
+        })
+        await book.save()
     }))
-    console.log(books);
 }
-fetchData()
+// fetchData()
 
 
